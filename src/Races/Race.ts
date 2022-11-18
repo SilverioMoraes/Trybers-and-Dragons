b24d82ec49563c687b09b73b889de764f035efe5
Silverio Moraes <silverstone191013@gmail.com>
2022-11-18 15:36:22 -0300

    req/01 :rocket:

---
+++ src/Races/Race.ts
@@ -0,0 +1,21 @@
+abstract class Race {
+  _name: string;
+  _dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  get name() { return this._name; }
+
+  get dexterity() { return this._dexterity; }
+
+  static createdRacesInstances() {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
+
+export default Race;
